6464119a0b33c86130073b447941b6b731d5f201
Aurelijus <auritoz95@gmail.com>
2019-05-25 23:44:24 +0300

    app.js clean up

--- src/app.js
+++ src/app.js
@@ -4,8 +4,6 @@ import {Provider} from 'react-redux';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import {addExpense} from './actions/expenses';
-import {setTextFilter} from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
@@ -14,7 +12,6 @@ const store = configureStore();
 
 store.subscribe(() => {
     const state = store.getState();
-    const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
 })
 
 store.dispatch(addExpense({description: 'water bill', amount: 800, createdAt: 3000}));
